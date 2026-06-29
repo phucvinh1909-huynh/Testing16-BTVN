a0e99528c311ceb89636868cb5ddbe36bd00f47c
Vinh <phucvinh1909@gmailcom>
2026-06-29 21:25:45 +0700

    Xong

---
+++ Bài Tập Java/baitap3cap1.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+
+public class baitap3cap1 {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Nhập so nguyen: ");
+        int soNguyen = input.nextInt();
+
+        int ketQua = 0;
+        for (int so = 0; so <= soNguyen; so++) {
+            if (so % 2 == 0) {
+                ketQua = ketQua + so;
+            }
+        }
+        System.out.println("Tong tat ca so chan la: " + ketQua);
+    }
+}
