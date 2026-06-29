c8798f8042cc2762a75675fcefadf726902e712d
Vinh <phucvinh1909@gmailcom>
2026-06-29 21:39:23 +0700

    Xong

---
+++ Bài Tập Java/baitap4cap1.java
@@ -0,0 +1,23 @@
+public class baitap4cap1 {
+}
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Nhap mot chuoi cac tu: ");
+    String chuoiTu = input.nextLine();
+
+    if (chuoiTu.isEmpty()) {
+        System.out.println("Chua nhap chuoi cac tu: ");
+        return;
+    }
+    int soKhoangCach = 0;
+
+    for (int i = 0; i < chuoiTu.length(); i++) {
+        if (chuoiTu.charAt(i) == ' ' && chuoiTu.charAt(i + 1) != ' ') {
+            soKhoangCach++;
+        }
+    }
+    int soTu = soKhoangCach + 1;
+    System.out.println("So tu trong chuoi la: " + soTu);
+}
+
+
