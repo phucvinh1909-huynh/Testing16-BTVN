ac136e47a9dd55522246e7178b19c6b9407dcd29
Vinh <phucvinh1909@gmailcom>
2026-06-30 08:55:54 +0700

    Xong

---
+++ Bài Tập Java/baitap5cap1.java
@@ -0,0 +1,20 @@
+
+public class baitap5cap1 {
+}
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+    System.out.print("Nhap mot chuoi cac tu: ");
+    String chuoiTu = input.nextLine();
+    String[] cacTu = chuoiTu.split(" ");
+
+    for (int i = 0; i < cacTu.length; i++) {
+        String chuoiTuHienTai = cacTu[i];
+
+        if (!chuoiTuHienTai.isEmpty()) {
+            char chuDau = chuoiTuHienTai.charAt(0);
+            char chuVietHoa = Character.toUpperCase(chuDau);
+
+            System.out.print(chuVietHoa + " ");
+        }
+    }
+}
