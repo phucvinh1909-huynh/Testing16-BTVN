044f850d012a1461d9326b384efe92566d08dce2
Vinh <phucvinh1909@gmailcom>
2026-06-30 09:53:35 +0700

    Xong

---
+++ BaiTapJavaCap2/baitap2cap2.java
@@ -0,0 +1,24 @@
+public class baitap2cap2 {
+}
+public static void main(String[] args) {
+    Scanner input = new Scanner(System.in);
+
+    System.out.print("Nhap vao mot chuoi tu: ");
+    String chuoiTu = input.nextLine();
+        chuoiTu = chuoiTu.toLowerCase();
+
+    boolean chuoiDoiXung = true;
+    int doDaiChuoi = chuoiTu.length();
+
+    for (int i = 0; i < doDaiChuoi / 2; i++) {
+        char kyTuDau = chuoiTu.charAt(i);
+        char kyTuCuoi = chuoiTu.charAt(doDaiChuoi - 1 - i);
+
+        if (kyTuDau != kyTuCuoi) {
+            chuoiDoiXung = false;
+            break;
+        }
+    }
+
+    System.out.println("Ket qua la: " + chuoiDoiXung);
+}
